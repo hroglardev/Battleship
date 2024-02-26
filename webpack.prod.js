e4e23d1c39809c551e47927ad7ab1dc9e7991bd1
Lucas Cubile <cubilelucas@gmail.com>
2024-02-26 12:04:24 -0300

    Project boilerplate

---
+++ webpack.prod.js
@@ -0,0 +1,19 @@
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common')
+const TerserPlugin = require('terser-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin')
+
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'source-map',
+  optimization: {
+    minimizer: [
+      new TerserPlugin(),
+      new CssMinimizerPlugin(),
+      new ImageMinimizerPlugin({
+        implementation: ImageMinimizerPlugin.sharpMinify
+      })
+    ]
+  }
+})
